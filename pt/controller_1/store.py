4c5aa95018583a9c3831eb031a96d8efd61c2cf0
AUMikkel <202007669@post.au.dk>
2025-04-22 15:26:55 +0200

    removed unused imports

--- pt/controller_1/store.py
+++ pt/controller_1/store.py
@@ -1,9 +1,8 @@
-import influxdb_client
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.client.exceptions import InfluxDBError
 import yaml
-from typing import Tuple
+
 
 class InfluxDBStore:
     def __init__(self):
@@ -13,13 +12,9 @@ class InfluxDBStore:
         self._url = config["url"]
         self._token = config["token"]
         self._bucket = config["bucket"]
-        #print(self._org, self._url, self._token, self._bucket)
-        write_client = influxdb_client.InfluxDBClient(
-            url=self._url, token=self._token, org=self._org
-        )
-        self._write_api = write_client.write_api(
-            write_options=SYNCHRONOUS
-        )
+        # print(self._org, self._url, self._token, self._bucket)
+        write_client = InfluxDBClient(url=self._url, token=self._token, org=self._org)
+        self._write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
     def get_config(self) -> dict:
         with open("config/config.yaml", "r") as file:
@@ -32,10 +27,13 @@ class InfluxDBStore:
         """
         try:
             self._write_api.write(bucket=self._bucket, org=self._org, record=record)
-            print(f"Successfully wrote point '{record.to_line_protocol()}' to the bucket {self._bucket}.")
+            print(
+                f"Successfully wrote point '{record.to_line_protocol()}' to the bucket {self._bucket}."
+            )
         except InfluxDBError as e:
-            print(f"Failed to write point '{record.to_line_protocol()}' with exception: {e.message}")
+            print(
+                f"Failed to write point '{record.to_line_protocol()}' with exception: {e.message}"
+            )
             print(f"Error Status Code: {e.status}")
             print(f"Error Message: {e.body}")
             print(f"Error Headers: {e.headers}")
-
