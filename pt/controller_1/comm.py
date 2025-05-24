6535ab58b8c13e51d312a12464243b44dcf124f4
AUMikkel <202007669@post.au.dk>
2025-05-24 15:12:28 +0200

    Restructured code to more easely reabeble and added heartbeats of 15 seconds to keep connection alive.

--- pt/controller_1/comm.py
+++ pt/controller_1/comm.py
@@ -1,7 +1,14 @@
 import stomp
-from config import get_stomp_url, get_stomp_user, get_stomp_password, get_stomp_port, get_stomp_actuator_ids
+from config import (
+    get_stomp_url,
+    get_stomp_user,
+    get_stomp_password,
+    get_stomp_port,
+    get_stomp_actuator_ids,
+)
 from pump import pump_water
 import time
+
 
 class stompClient(stomp.ConnectionListener):
     def __init__(self, func):
@@ -11,35 +18,41 @@ class stompClient(stomp.ConnectionListener):
         self._password = get_stomp_password()
         self._port = get_stomp_port()
         self._ids = get_stomp_actuator_ids()
-        self.conn = stomp.Connection([(self._url, self._port)])
-        self.conn.set_listener('', self)
+        self.heartbeats = (
+            15000,
+            15000,
+        )  # Heartbeat interval in milliseconds (from client to server, from server to client)
+        self.conn = stomp.Connection(
+            [(self._url, self._port)], heartbeats=self.heartbeats
+        )
+        self.conn.set_listener("", self)
         self.conn.start()
         self.conn.connect(self._user, self._password, wait=True)
 
     def on_connected(self, frame):
-        print('Connected: %s' % frame.body)
+        print("Connected: %s" % frame.body)
         for index, id in enumerate(self._ids):
             queue_destination = f"actuator.{str(id)}.water"
             try:
-                self.conn.subscribe(destination=queue_destination, id=index, ack='auto')
+                self.conn.subscribe(destination=queue_destination, id=index, ack="auto")
                 print(f"Subscribed to {queue_destination} with id {index}")
             except Exception as e:
                 print(f"Error subscribing to {queue_destination}: {e}")
                 continue
 
     def on_error(self, frame):
-        print('Error: %s' % frame.body)
+        print("Error: %s" % frame)
 
     def on_message(self, frame):
-        print('Message: %s' % frame.body)
-        # We need to command <pump> <time>
+        print("STOMP Command Message: %s" % frame.body)
+        # We need to command [WATER]<pump_pin> <duration>
         command = frame.body.split("[WATER]")[1]
-        command_list = command.strip().split()
-        
+        command_list = command.split(" ")
+
         if command_list[0] == "water":
             self._func(int(command_list[1]), int(command_list[2]))
-        else:
-            self._func(int(command_list[0]), int(command_list[1]))
+        else:  # pin, duration
+            self._func(int(command_list[1]), int(command_list[0]))
 
     def send_message(self, destination, message):
         self.conn.send(destination=destination, body=message)
